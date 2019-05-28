fae1a70afa732f8e4322c8f1d35b3bbaa644d3cf
Hrishi <hrishipatel99@gmail.com>
2019-05-28 16:33:48 +0530

    add pie chart

--- activity.py
+++ activity.py
@@ -14,11 +14,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-"""HelloWorld Activity: A case study for developing an activity."""
 
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, Gdk, Pango
+from gi.repository import Gtk, Gdk, Pango, GObject
+import logging
 
 from gettext import gettext as _
 
@@ -27,15 +27,48 @@ from sugar3.graphics.toolbarbox import ToolbarBox
 from sugar3.activity.widgets import ActivityButton
 from sugar3.activity.widgets import StopButton
 from sugar3.datastore import datastore
-
 from jarabe.model import bundleregistry
+
+from charts import Chart
+from readers import JournalReader
 from collections import Counter
+import utils
+import os
+
+from jarabe.journal.listmodel import ListModel
+from jarabe.journal.palettes import ObjectPalette, BuddyPalette
+from jarabe.journal import model
+from jarabe.journal import misc
+from jarabe.journal import journalwindow
+from jarabe.journal.listview import ListView
+
+
+# GUI colors
+_COLOR1 = utils.get_user_fill_color()
+_COLOR2 = utils.get_user_stroke_color()
+_WHITE = Gdk.color_parse("white")
+
+# paths
+_ACTIVITY_DIR = os.path.join(activity.get_activity_root(), "data/")
+_CHART_FILE = utils.get_chart_file(_ACTIVITY_DIR)
+
+# logging 
+_logger = logging.getLogger('analyze-journal-activity')
+_logger.setLevel(logging.DEBUG)
+logging.basicConfig()
 
 class DashboardActivity(activity.Activity):
 
     def __init__(self, handle):
 
         activity.Activity.__init__(self, handle)
+
+        self.current_chart = None
+        self.x_label = ""
+        self.y_label = ""
+        self.chart_color = utils.get_user_fill_color('str')
+        self.chart_line_color = utils.get_user_stroke_color('str')
+        self.chart_data = []
 
         # toolbar with the new toolbar redesign
         toolbar_box = ToolbarBox()
@@ -71,6 +104,15 @@ class DashboardActivity(activity.Activity):
         vbox_total_activities.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(1, 1, 1, 1))
         vbox_journal_entries.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(1, 1, 1, 1))
         vbox_total_contribs.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(1, 1, 1, 1))
+        
+        hbox_tree = Gtk.HBox()
+        hbox_tree.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(1, 1, 1, 1))
+
+        hbox_pie = Gtk.HBox()
+        hbox_pie.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(1, 1, 1, 1))
+
+        label_dashboard = Gtk.Label()
+        label_dashboard.set_markup(_("<b>Dashboard</b>"))
 
         label_dashboard = Gtk.Label()
         label_dashboard.set_markup(_("<b>Dashboard</b>"))
@@ -91,7 +133,7 @@ class DashboardActivity(activity.Activity):
         label_journal_entries = Gtk.Label()
         vbox_journal_entries.add(label_journal_entries)
 
-        # label for ??
+        # label for files
         label_CE = Gtk.Label()
         label_CE.set_markup(_("<b>Total Files</b>"))
         vbox_total_contribs.add(label_CE)
@@ -99,12 +141,35 @@ class DashboardActivity(activity.Activity):
         label_contribs = Gtk.Label()
         vbox_total_contribs.add(label_contribs)
 
-        # empty label
-
+        # empty label(s)
         label_empt = Gtk.Label("")
+        label_placeholder = Gtk.Label("Placeholder for Journal Entries")
+
+        # pie chart
+
+        treeview = ListView(self, enable_multi_operations=True)
+        tree_view = treeview.tree_view
+        treeview.show_all()
+
+        self.labels_and_values = ChartData(self)
+        hbox_tree.add(treeview)
+        self.labels_and_values.connect("label-changed", self._label_changed)
+        self.labels_and_values.connect("value-changed", self._value_changed)
+
+        eventbox = Gtk.EventBox()
+        self.charts_area = ChartArea(self)
+        self.charts_area.connect('size_allocate', self._chart_size_allocate)
+        eventbox.modify_bg(Gtk.StateType.NORMAL, _WHITE)
+        eventbox.add(self.charts_area)
+        hbox_pie.add(eventbox)
+
+        reader = JournalReader()
+        self._graph_from_reader(reader)
+        self.current_chart = Chart("pie")
+        self.update_chart()
 
         # font
-        font_main = Pango.FontDescription("Dejavu Sans Mono 14")
+        font_main = Pango.FontDescription("Granada 14")
         label_JE.modify_font(font_main)
         label_CE.modify_font(font_main)
         label_TA.modify_font(font_main)
@@ -125,6 +190,7 @@ class DashboardActivity(activity.Activity):
                         'text/plain']
         label_test = Gtk.Label()
 
+        # to check journal entries which are only a file
         for dsobject in dsobjects:
             if dsobject.metadata['mime_type'] in mime_types:
                 files_list.append(dsobject.metadata['title'])
@@ -134,10 +200,239 @@ class DashboardActivity(activity.Activity):
         label_journal_entries.set_text(str(journal_entries))
         label_contribs.set_text(str(len(files_list)))
 
-        grid.attach(label_dashboard, 2, 2, 20, 20)
+        grid.attach(label_dashboard, 1, 2, 20, 20)
         grid.attach_next_to(label_empt, label_dashboard, Gtk.PositionType.BOTTOM, 6, 1)
-        grid.attach_next_to(vbox_total_activities, label_empt, Gtk.PositionType.RIGHT, 50, 50)
-        grid.attach_next_to(vbox_journal_entries, vbox_total_activities, Gtk.PositionType.RIGHT, 50, 50)
-        grid.attach_next_to(vbox_total_contribs, vbox_journal_entries, Gtk.PositionType.RIGHT, 50, 50)
+        grid.attach_next_to(vbox_total_activities, label_empt, Gtk.PositionType.RIGHT, 50, 35)
+        grid.attach_next_to(vbox_journal_entries, vbox_total_activities, Gtk.PositionType.RIGHT, 50, 35)
+        grid.attach_next_to(vbox_total_contribs, vbox_journal_entries, Gtk.PositionType.RIGHT, 50, 35)
+        grid.attach_next_to(hbox_tree, vbox_total_activities, Gtk.PositionType.BOTTOM, 75, 100)
+        grid.attach_next_to(hbox_pie, hbox_tree, Gtk.PositionType.RIGHT, 75, 100)
         grid.show_all()
-    
+
+    def _chart_size_allocate(self, widget, allocation):
+        self._render_chart()
+
+    def _render_chart(self):
+        if self.current_chart is None or self.charts_area is None:
+            return
+
+        try:
+            # Resize the chart for all the screen sizes
+            alloc = self.get_allocation()
+            #alloc = self.charts_area.get_allocation()
+
+            new_width = alloc.width - 300
+            new_height = alloc.height - 300
+
+            self.current_chart.width = new_width
+            self.current_chart.height = new_height
+
+            # Set options
+            self.current_chart.set_color_scheme(color=self.chart_color)
+            self.current_chart.set_line_color(self.chart_line_color)
+
+            if self.current_chart.type == "pie":
+                self.current_chart.render(self)
+            else:
+                self.current_chart.render()
+            self.charts_area.queue_draw()
+
+        except (ZeroDivisionError, ValueError):
+            pass
+
+        return False
+
+    def _graph_from_reader(self, reader):
+        self.labels_and_values.model.clear()
+        self.chart_data = []
+
+        chart_data = reader.get_chart_data()
+        horizontal, vertical = reader.get_labels_name()
+
+        # Load the data
+        for row  in chart_data:
+            self._add_value(None, 
+                                label=row[0], value=float(row[1]))
+
+            self.update_chart()
+
+    def _add_value(self, widget, label="", value="0.0"):
+        data = (label, float(value))
+        if not data in self.chart_data:
+            pos = self.labels_and_values.add_value(label, value)
+            self.chart_data.insert(pos, data)
+            self._update_chart_data()
+
+    def update_chart(self):
+        if self.current_chart:
+            self.current_chart.data_set(self.chart_data)
+            #self.current_chart.set_title(self.metadata["title"])
+            self.current_chart.set_x_label(self.x_label)
+            self.current_chart.set_y_label(self.y_label)
+            self._render_chart()
+
+    def _update_chart_data(self):
+        if self.current_chart is None:
+            return
+        self.current_chart.data_set(self.chart_data)
+        self._update_chart_labels()
+
+    def _update_chart_labels(self, title=""):
+        if self.current_chart is None:
+            return
+        self.current_chart.set_title(title)
+        self.current_chart.set_x_label(self.x_label)
+        self.current_chart.set_y_label(self.y_label)
+        self._render_chart()
+
+    def _value_changed(self, treeview, path, new_value):
+        path = int(path)
+        self.chart_data[path] = (self.chart_data[path][0], float(new_value))
+        self._update_chart_data()
+
+    def _label_changed(self, treeview, path, new_label):
+        path = int(path)
+        self.chart_data[path] = (new_label, self.chart_data[path][1])
+        self._update_chart_data()
+
+
+class ChartArea(Gtk.DrawingArea):
+
+    def __init__(self, parent):
+        """A class for Draw the chart"""
+        super(ChartArea, self).__init__()
+        self._parent = parent
+        self.add_events(Gdk.EventMask.EXPOSURE_MASK | Gdk.EventMask.VISIBILITY_NOTIFY_MASK)
+        self.connect("draw", self._draw_cb)
+
+    def _draw_cb(self, widget, context):
+        alloc = self.get_allocation()
+
+        # White Background:
+        context.rectangle(0, 0, alloc.width, alloc.height)
+        context.set_source_rgb(255, 255, 255)
+        context.fill()
+
+        # Paint the chart:
+        chart_width = self._parent.current_chart.width
+        chart_height = self._parent.current_chart.height
+
+        cxpos = alloc.width / 2 - chart_width / 2
+        cypos = alloc.height / 2 - chart_height / 2
+
+        context.set_source_surface(self._parent.current_chart.surface,
+                                   cxpos,
+                                   cypos)
+        context.paint()
+
+
+class ChartData(Gtk.TreeView):
+
+    __gsignals__ = {
+             'label-changed': (GObject.SignalFlags.RUN_FIRST, None, [str, str], ),
+             'value-changed': (GObject.SignalFlags.RUN_FIRST, None, [str, str], ), }
+
+    def __init__(self, activity):
+
+        GObject.GObject.__init__(self)
+
+
+        self.model = Gtk.ListStore(str, str)
+        self.set_model(self.model)
+
+        self._selection = self.get_selection()
+        self._selection.set_mode(Gtk.SelectionMode.SINGLE)
+
+        # Label column
+
+        column = Gtk.TreeViewColumn(_("Label"))
+        label = Gtk.CellRendererText()
+        label.set_property('editable', True)
+        label.connect("edited", self._label_changed, self.model)
+
+        column.pack_start(label, True)
+        column.add_attribute(label, 'text', 0)
+        self.append_column(column)
+
+        # Value column
+
+        column = Gtk.TreeViewColumn(_("Value"))
+        value = Gtk.CellRendererText()
+        value.set_property('editable', True)
+        value.connect("edited", self._value_changed, self.model, activity)
+
+        column.pack_start(value, True)
+        column.add_attribute(value, 'text', 1)
+
+        self.append_column(column)
+        self.set_enable_search(False)
+
+        self.show_all()
+
+    def add_value(self, label, value):
+        treestore, selected = self._selection.get_selected()
+        if not selected:
+            path = 0
+
+        elif selected:
+            path = int(str(self.model.get_path(selected))) + 1
+        try:
+            _iter = self.model.insert(path, [label, value])
+        except ValueError:
+            _iter = self.model.append([label, str(value)])
+
+
+        self.set_cursor(self.model.get_path(_iter),
+                        self.get_column(1),
+                        True)
+
+        _logger.info("Added: %s, Value: %s" % (label, value))
+
+        return path
+
+    def remove_selected_value(self):
+        model, iter = self._selection.get_selected()
+        value = (self.model.get(iter, 0)[0], float(self.model.get(iter, 1)[0]))
+        _logger.info('VALUE: ' + str(value))
+        self.model.remove(iter)
+
+        return value
+
+    def _label_changed(self, cell, path, new_text, model):
+        _logger.info("Change '%s' to '%s'" % (model[path][0], new_text))
+        model[path][0] = new_text
+
+        self.emit("label-changed", str(path), new_text)
+
+    def _value_changed(self, cell, path, new_text, model, activity):
+        _logger.info("Change '%s' to '%s'" % (model[path][1], new_text))
+        is_number = True
+        number = new_text.replace(",", ".")
+        try:
+            float(number)
+        except ValueError:
+            is_number = False
+
+        if is_number:
+            decimals = utils.get_decimals(str(float(number)))
+            new_text = locale.format('%.' + decimals + 'f', float(number))
+            model[path][1] = str(new_text)
+
+            self.emit("value-changed", str(path), number)
+
+        elif not is_number:
+            alert = Alert()
+
+            alert.props.title = _('Invalid Value')
+            alert.props.msg = \
+                           _('The value must be a number (integer or decimal)')
+
+            ok_icon = Icon(icon_name='dialog-ok')
+            alert.add_button(Gtk.ResponseType.OK, _('Ok'), ok_icon)
+            ok_icon.show()
+
+            alert.connect('response', lambda a, r: activity.remove_alert(a))
+
+            activity.add_alert(alert)
+
+            alert.show()
